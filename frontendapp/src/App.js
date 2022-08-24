1bfa53211c912a0bc981771d961d23a2a17ce962
ahmed trabelsi <hmayda123trabelsi@gmail.com>
2022-08-24 23:29:28 +0200

    first apicall

--- frontendapp/src/App.js
+++ frontendapp/src/App.js
@@ -1,20 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
-import { useState } from 'react';
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import { useState } from "react";
+import { ThemeConsumer } from "react-bootstrap/esm/ThemeProvider";
+const UNSPLACH_KEY=process.env.REACT_APP_UNSPLACH_KEY;
 function App() {
-  const [word,setword]=useState('');
-  const handleSearchSubmit=(e)=>{
+  const [word, setword] = useState("");
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-  }
-  
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLACH_KEY}`)
+    .Then((res)=>res.json())
+    .then((data)=>{console.log(data);})
+    .catch((err)=>{console.log(err);})
+  };
+
   return (
     <div className="App">
-      < Header title="images haha "/>
-      <Search word={word} setword={setword} handleSubmit={handleSearchSubmit}/>
+      <Header title="images haha " />
+      <Search word={word} setword={setword} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
