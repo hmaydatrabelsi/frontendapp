40394e382bbbf7356a99dc03f0b142b535400297
ahmed trabelsi <hmayda123trabelsi@gmail.com>
2022-08-26 22:17:15 +0200

    esâce

--- frontendapp/src/App.js
+++ frontendapp/src/App.js
@@ -2,18 +2,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import { useState } from "react";
-import { ThemeConsumer } from "react-bootstrap/esm/ThemeProvider";
-const UNSPLACH_KEY=process.env.REACT_APP_UNSPLACH_KEY;
+const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY;
 function App() {
   const [word, setword] = useState("");
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLACH_KEY}`)
-    .Then((res)=>res.json())
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    .then((res)=>res.json())
     .then((data)=>{console.log(data);})
     .catch((err)=>{console.log(err);})
-  };
+    setword('');
+  }
 
   return (
     <div className="App">
